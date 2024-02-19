59a3e8935f9d1d8c7827057dec3faa29ae9da72f
littlewhitehhh <1021014610@qq.com>
2024-02-19 15:43:34 +0800

    feat: install prettier

---
+++ src/components/testPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>testPage</div>
+  <HelloWorld msg="hhh"></HelloWorld>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./HelloWorld.vue";
+</script>
+
+<style></style>
